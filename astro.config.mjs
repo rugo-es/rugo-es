e39f2cfde36857e26ea286a545f39848855f63f3
Rubén González Martín <ruben.gonzalez@afterbanks.com>
2025-02-05 14:14:40 +0100

    Custom domain - tech.rugo.es

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rugo-es.github.io',
-  base: 'personal-page',
+  site: 'https://tech.rugo.es'
 });
